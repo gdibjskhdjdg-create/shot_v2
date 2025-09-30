1b6ceec30944a4815c146e2ffb7fae7b5d31bb03
gdibjskhdjdg <gdibjskhdjdg@gmail.com>
2025-09-30 10:17:43 +0000

    fixed bug

--- modules/controllers/tag/Tag.controller.js
+++ modules/controllers/tag/Tag.controller.js
@@ -17,7 +17,7 @@ const listTags = async (req, res) => {
         sortACS = "DESC",
     } = getDataFromReqQuery(req);
 
-    const tags = await TagService.getTags({ search, categoryId, excludeId, type, page, take, shotUsageCount: true, sortKey, sortACS });
+    const tags = await TagService.fetch({ search, categoryId, excludeId, type, page, take, shotUsageCount: true, sortKey, sortACS });
     return BaseController.ok(res, { tags: Tag_DTO.create(tags.rows), count: tags.count });
 }
 
@@ -29,33 +29,33 @@ const searchTags = async (req, res) => {
         page = 1,
         take = 10
     } = getDataFromReqQuery(req);
-    const tags = await TagService.getTags({ search, excludeId, type, page, take });
+    const tags = await TagService.fetch({ search, excludeId, type, page, take });
     return BaseController.ok(res, { tags: Tag_DTO.create(tags.rows), count: tags.count });
 }
 
 const getTagDetails = async (req, res) => {
     const { tagId } = req.params;
-    const tag = await TagService.getTagDetail(tagId);
+    const tag = await TagService.fetchDetails(tagId);
     return BaseController.ok(res, tag);
 }
 
 const getTagShots = async (req, res) => {
     const { tagId } = req.params;
     const query = getDataFromReqQuery(req);
-    const tag = await TagService.getShotsOfTag(tagId, query);
+    const tag = await TagService.fetchShots(tagId, query);
     return BaseController.ok(res, tag);
 }
 
 const createNewTag = async (req, res) => {
     const body = req.body;
     const data = TagValidation.createTag(body);
-    const tag = await TagService.createTag(data);
+    const tag = await TagService.add(data);
     return BaseController.ok(res, Tag_DTO.create(tag));
 }
 
 const mergeTags = async (req, res) => {
     const { sourceTagId, targetTagId } = req.params
-    const hasAffected = await TagService.mergeTag(sourceTagId, targetTagId)
+    const hasAffected = await TagService.combine(sourceTagId, targetTagId)
     return BaseController.ok(res, {hasAffected});
 
 }
@@ -64,20 +64,20 @@ const updateTagInfo = async (req, res) => {
     const body = req.body;
     const { tagId } = req.params;
     const data = TagValidation.createTag(body);
-    const tag = await TagService.editTag(tagId, data);
+    const tag = await TagService.update(tagId, data);
     return BaseController.ok(res, Tag_DTO.create(tag));
 
 }
 
 const removeShotFromTag = async (req, res) => {
     const { tagId, shotId } = req.params
-    await TagService.detachShotFromTag(tagId, shotId)
+    await TagService.unlinkShot(tagId, shotId)
     return BaseController.ok(res);
 }
 
 const permanentlyDeleteTag = async (req, res) => {
     const { tagId } = req.params
-    await TagService.deleteTag(tagId)
+    await TagService.remove(tagId)
     return BaseController.ok(res);
 }
 
