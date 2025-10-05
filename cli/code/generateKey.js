8d989aa4f0bbf3f4121c249bd84e0a2048ecad7a
harani <ehsan.harani@gmail.com>
2025-10-05 17:04:44 +0330

    delete cli>code  files

--- cli/code/generateKey.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-
-const ENC_KEY = crypto.randomBytes(32).toString('hex');
-
-console.log('کلید رمزنگاری شما:', ENC_KEY);
-console.log('طول کلید (بایت):', Buffer.from(ENC_KEY, 'hex').length);
